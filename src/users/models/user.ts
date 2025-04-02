7c2d828f664ffac7cd75d6a5a498d3bd9e5fa64d
SJ Kim <blackmilk274@gmail.com>
2025-04-02 13:15:49 +0000

    User 모델 타입 정의
    - sequelize 사용시 모델타입을 정의해주지 않으면 타입 체킹이 되지 않음
    - 따라서 User 모델 타입 정의 및 관련 속성 타입 정의
    
    Related #3

--- src/users/models/user.ts
+++ src/users/models/user.ts
@@ -1,21 +1,51 @@
 import { sequelize } from '../../config/db';
-import { DataTypes } from 'sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
 
 type UserColumns = {
   id: string;
   name: string;
 };
 
-const User = sequelize.define('User', {
+type UserAttributes = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+type UserCreationAttributes = Optional<UserAttributes, 'createdAt'| 'updatedAt'>;
+
+class User extends Model<UserAttributes, UserCreationAttributes> {
+  declare id: string;
+  declare name: string;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+User.init({
   id: {
     type: DataTypes.STRING,
     primaryKey: true,
+    allowNull: false,
   },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'User',
+  timestamps: true,
 });
 
-export {User};
+
+export { User };
 export type { UserColumns };
