46c717896d623719b357769d08fab0985ac4f1da
sunmon <blackmilk274@gmail.com>
2025-04-07 20:29:06 +0900

    refactor: 팔로우 레포지토리 분리
    Related #3

--- src/follows/models/follow.ts
+++ src/follows/models/follow.ts
@@ -3,7 +3,6 @@ import { DataTypes, Model, Optional } from 'sequelize';
 
 // follower: 나를 팔로우 하는 사람
 // follwing: 내가 팔로잉 하는 사람
-
 type FollowAttributes = {
   id: number;
   userId: string; // 팔로우 하는 사람
@@ -58,18 +57,5 @@ Follow.init(
   },
 );
 
-function addFollow({ userId, targetId }: FollowAttributes) {
-  return Follow.create({ userId, targetId });
-}
-
-// userId가 팔로잉 하는 사람
-function getFollowings(userId: string) {
-  return Follow.findAll({ where: { userId } });
-}
-
-// targetId를 팔로우 하는 사람
-function getFollowers(targetId: string) {
-  return Follow.findAll({ where: { targetId } });
-}
-
-export { addFollow, getFollowers, getFollowings };
+export { Follow };
+export type { FollowAttributes, FollowCreationAttributes };
