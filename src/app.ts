e5090599dee181f4f16805be8718f49654fc126d
sunmon <blackmilk274@gmail.com>
2025-04-10 00:40:07 +0900

    refactor: 사용하지 않는 import 삭제
    Related #3

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,6 @@
 // @ts-nocheck
 
 import express from 'express';
-import UserService from './users/services/user_service';
-import PostService from './post/services/post_service';
-import FollowService from './follow/services/follow_service';
-import NewsfeedService from './newsfeed/services/newsfeed_service';
 import { connectDB, syncDB } from './config/db';
 
 import userRouter from './users/routes';
@@ -17,7 +13,6 @@ const port = 3333;
 
 initDB();
 
-// TODO 엔드포인트 라우터 분리
 app.get('/', (req, res) => {
   res.set({ 'Content-Type': 'text/html; charset=utf-8' });
   res.end('hello world!!!!');
