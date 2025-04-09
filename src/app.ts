97521838d5b0e8c960824872a37adcef91348c17
sunmon <blackmilk274@gmail.com>
2025-04-10 00:10:32 +0900

    fix: app.ts 내부 import 수정
    - 서비스 분리에 따라 import 문구 수정
    
    Related #3

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,17 @@
 // @ts-nocheck
 
 import express from 'express';
-import { getUser, addUser } from './users/models/user';
-import { getPost } from './post/models/post';
-import {
-  addFollow,
-  getFollowers,
-  getFollowings,
-} from './follow/models/follow';
+import UserService from './users/services/user_service';
+import PostService from './post/services/post_service';
+import FollowService from './follow/services/follow_service';
+import NewsfeedService from './newsfeed/services/newsfeed_service';
 import { connectDB, syncDB } from './config/db';
-import {
-  addPost,
-  getAllPosts,
-  getAllPostsByUserId,
-} from './post/repositories/post_repository';
 
 const app = express();
 const port = 3333;
 
 initDB();
 
-// TODO 테스트 코드 작성
 // TODO 엔드포인트 라우터 분리
 app.get('/', (req, res) => {
   res.set({ 'Content-Type': 'text/html; charset=utf-8' });
@@ -35,13 +26,13 @@ app.use(express.json());
 
 app.post('/users', async (req, res) => {
   const { userId, name } = req.body;
-  const user = await addUser({ id: userId, name });
+  const user = await UserService.join({ id: userId, name });
   return res.json(user);
 });
 
 app.get('/users/:userId', async (req, res) => {
   const userId = req.params.userId;
-  const user = await getUser(userId);
+  const user = await UserService.getUser(userId);
   return res.json(user);
 });
 
@@ -49,40 +40,40 @@ app.get('/users/:userId', async (req, res) => {
 app.post('/follow/:targetId', async (req, res) => {
   const targetId = req.params.targetId;
   const userId = req.body.userId;
-  const follow = await addFollow({ userId, targetId });
+  const follow = await FollowService.addFollow({ userId, targetId });
   return res.json(follow);
 });
 
 // user의 팔로워 목록 반환
 app.get('/follow/:userId/followers', async (req, res) => {
   const userId = req.params.userId;
-  const followers = await getFollowers(userId);
+  const followers = await FollowService.getFollowers(userId);
   return res.json(followers);
 });
 
 // user가 팔로잉하는 목록 반환
 app.get('/follow/:userId/followings', (req, res) => {
   const userId = req.params.userId;
-  const followings = getFollowings(userId);
+  const followings = FollowService.getFollowings(userId);
   return res.json(followings);
 });
 
 // post
 app.get('/posts/:postId', async (req, res) => {
   const postId = req.params.postId;
-  const post = await getPost(postId);
+  const post = await PostService.getPost(postId);
 
   return res.json(post);
 });
 
 app.post('/posts', async (req, res) => {
   const { title, content, userId } = req.body;
-  const post = await addPost({ title, content, userId });
+  const post = await PostService.addPost({ title, content, userId });
   return res.json(post);
 });
 
 app.get('/posts', async (req, res) => {
-  const posts = await getAllPosts();
+  const posts = await PostService.getAllPosts();
   return res.json(posts);
 });
 
@@ -94,7 +85,7 @@ app.get('/posts', async (req, res) => {
 
 // TODO: 나중에 헤더에 토큰넣으면 url 수정
 app.get('/newsfeed/:userId', async (req, res) => {
-  const newfeed = await getAllPostsByUserId(req.params.userId);
+  const newfeed = await NewsfeedService.getNewsfeedByUser(req.params.userId);
   return res.json(newfeed);
 });
 
