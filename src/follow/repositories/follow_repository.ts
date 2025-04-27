ea7a6cde0df86311be4d279a2d2e0002427d62a3
sunmon <blackmilk274@gmail.com>
2025-04-27 18:17:35 +0900

    refactor: follow repository 개별 export 삭제
    - 마지막에 export default로 묶어서 내보내기때문에 삭제함
    
    Related #8

--- src/follow/repositories/follow_repository.ts
+++ src/follow/repositories/follow_repository.ts
@@ -1,14 +1,14 @@
 import { Follow, FollowCreationAttributes } from '../models/follow';
 
-export function addFollow({ userId, targetId }: FollowCreationAttributes) {
+function addFollow({ userId, targetId }: FollowCreationAttributes) {
   return Follow.create({ userId, targetId });
 } // targetId를 팔로우 하는 사람
 // userId가 팔로잉 하는 사람
-export function getFollowings(userId: string) {
+function getFollowings(userId: string) {
   return Follow.findAll({ where: { userId } });
 }
 
-export function getFollowers(targetId: string) {
+function getFollowers(targetId: string) {
   return Follow.findAll({ where: { targetId } });
 }
 
