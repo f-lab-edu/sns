98b2bf1d0aa21ac6862064905f8d6cb605f68c11
sunmon <blackmilk274@gmail.com>
2025-05-01 23:01:05 +0900

    feat: 이미지 업로드 미들웨어
    - multer 라이브러리 추가
    - 이미지 업로드 미들웨어 추가
    
    Related #9

--- src/post/routes/index.ts
+++ src/post/routes/index.ts
@@ -2,6 +2,9 @@
 
 import { Router } from 'express';
 import PostService from '../services/post_service';
+import path from 'node:path';
+import fs from 'node:fs';
+import multer from 'multer';
 
 const router = Router();
 
@@ -26,5 +29,41 @@ router.post('/', async (req, res) => {
   const post = await PostService.addPost({ title, content, userId });
   return res.json(post);
 });
+
+// 이미지를 로컬에 업로드
+const upload = initMulter();
+router.post('/upload/image', upload.single('file'), async (req, res) => {
+  // TODO do something
+});
+
+function initMulter() {
+  const uploadDir = path.join(__dirname, 'src/uploads/images');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  // 파일 업로드
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      const ext = path.extname(file.originalname);
+      cb(null, uniqueSuffix + ext);
+    },
+  });
+
+  return multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 }, // 1MB
+    fileFilter: function (req, file, cb) {
+      if (!file.mimetype.startsWith('image/')) {
+        return cb(new Error('FILE_TYPE_NOT_ALLOWED'));
+      }
+      cb(null, true);
+    },
+  });
+}
 
 export default router;
