6ec7a30fc049b01c811fa347115ad3544305aca6
sunmon <blackmilk274@gmail.com>
2025-05-01 17:06:07 +0900

    feat: Post 모델에 images[] 필드 추가
    - 사진 url을 저장할 iamges 필드 추가
    - 사진 url을 포함하여 업로드를 테스트하는 코드 추가
    
    Related #9

--- src/post/models/post.ts
+++ src/post/models/post.ts
@@ -8,11 +8,12 @@ type PostAttributes = {
   userId: string;
   createdAt: Date;
   updatedAt: Date;
+  images?: string[]; // REVIEW - 사진이 없는 상태를 null로 할지, 빈 배열로 할지, undefined로 할지 고민
 };
 
 type PostCreationAttributes = Optional<
   PostAttributes,
-  'id' | 'createdAt' | 'updatedAt'
+  'id' | 'createdAt' | 'updatedAt' | 'images'
 >;
 
 type PostUpdateAttributes = Partial<
@@ -26,6 +27,11 @@ class Post extends Model<PostAttributes, PostCreationAttributes> {
   declare userId: string;
   declare createdAt: Date;
   declare updatedAt: Date;
+  // REVIEW - 이미지 url을 배열로 가지고 있는게 적합할지?
+  // 배열을 가지고 있는게 데이터베이스 정규화에 적합한거같지는 않음
+  // 정규화를 만족하도록 하려면 image 필드로 변경하여 한번에 한개의 값만 가지도록 하고, 같은 postId에 대해서 여러 row를 추가해야 함
+  // 하지만 그렇게 하면 pk인 id가 pk로서의 역할을 상실하게 됨
+  declare images?: string[];
 }
 
 Post.init(
@@ -54,6 +60,10 @@ Post.init(
     updatedAt: {
       type: DataTypes.DATE,
       allowNull: false,
+    },
+    images: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
     },
   },
   {
