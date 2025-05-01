603ac2a53d8adc7d84827481ee0dcf7c5ce5d559
sunmon <blackmilk274@gmail.com>
2025-05-01 21:37:28 +0900

    test: PostImage 추가 테스트 코드
    - 게시글 이미지 업로드 기능 테스트코드 추가
    - 게시글 이미지 업로드하는 기능 추가
    
    Related #9

--- src/test/post_image_service.test.ts
+++ src/test/post_image_service.test.ts
@@ -1,10 +1,32 @@
 import PostImageService from '../post/services/post_image_service';
+import PostService from '../post/services/post_service';
 import { initDB } from '../config/db';
 import { beforeAll, expect } from '@jest/globals';
 
 describe('PostService', () => {
   beforeAll(async () => {
     await initDB();
+  });
+
+  test('이미지를 업로드한다', async () => {
+    const result = PostImageService.uploadImage();
+    expect(result).not.toBeNull();
+  });
+
+  test('이미지 url을 전달받아 게시글 이미지를 업로드한다', async () => {
+    //given
+    const post = await PostService.addPost({
+      title: 'title1',
+      content: 'content1',
+      userId: 'user123',
+    });
+
+    //when
+    const result = await PostImageService.createPostImage(post.id, '/test/url');
+
+    //then
+    expect(result).not.toBeNull();
+    expect(result.url).toBe('/test/url');
   });
 
   afterEach(() => {
